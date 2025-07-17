f7dc5613a48c3220b246c1b515c8d499d9510085
Scott Dunkley <scottdunkley@gmail.com>
2025-07-18 00:56:41 +0100

    Improved third party dependency handling, using FetchContent_Declare as do not need to include or use git submodules..for now.

--- Source/ThirdParty/Mimalloc.cmake
+++ Source/ThirdParty/Mimalloc.cmake
@@ -1,1 +1,22 @@
-#uoengine_add_thirdparty_library(DotNetSDK "Microsoft.NETCore.App.Host.win-x64/9.0.2/runtimes/win-x64/native")
+FetchContent_Declare(
+  mimalloc
+  GIT_REPOSITORY https://github.com/microsoft/mimalloc.git
+  GIT_TAG        dfa50c37d951128b1e77167dd9291081aa88eea4 # v3.1.5
+)
+
+FetchContent_MakeAvailable(mimalloc)
+
+#add_subdirectory(${mimalloc_SOURCE_DIR} ${mimalloc_BINARY_DIR} EXCLUDE_FROM_ALL)
+#set_target_properties(mimalloc-static PROPERTIES FOLDER "ThirdParty")
+
+get_directory_property(MIMALLOC_TARGETS DIRECTORY ${mimalloc_SOURCE_DIR} BUILDSYSTEM_TARGETS)
+
+foreach(tgt IN LISTS MIMALLOC_TARGETS)
+
+    #if(TARGET mimalloc-static)
+     #   set_target_properties(mimalloc-static PROPERTIES FOLDER "ThirdParty")
+   # else()
+        set_target_properties(${tgt} PROPERTIES FOLDER "ThirdParty")
+   # endif()
+
+endforeach()
