093c7146783053dcb542371c4cf0c8fb76202e5a
Scott Dunkley <scottdunkley@gmail.com>
2025-07-20 14:15:34 +0100

    Make Generate.bat handle being ran via a github actions or not. Added msbuild to github action to build (and get cmake?).

--- Generate.bat
+++ Generate.bat
@@ -1,10 +1,22 @@
+@echo off
 
 set ROOT=%~dp0
 
 set BUILD_DIR="%ROOT%\Intermediate\Win64"
 
-set CMAKE_BIN=%ROOT%Tools\cmake-4.0.0-rc3-windows-x86_64\bin\cmake.exe
+cmake --version >nul 2>&1
+
+if %ERRORLEVEL%==0 (
+    set CMAKE_BIN=cmake
+) else (
+    set CMAKE_BIN=%ROOT%Tools\cmake-4.0.0-rc3-windows-x86_64\bin\cmake.exe
+)
+
+
+echo Using cmake at %CMAKE_BIN%
 
 %CMAKE_BIN% -B %BUILD_DIR% -G "Visual Studio 17 2022" -DUSE_LIVEPP=ON
 
-pause
+if /I NOT "%GITHUB_ACTIONS%"=="true" (
+    pause
+)
