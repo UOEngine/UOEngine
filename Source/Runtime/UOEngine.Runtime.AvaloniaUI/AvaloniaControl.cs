cd4feb377e66e5a73201bd6dbfc1e017ca2590b5
Scott Dunkley <scottdunkley@gmail.com>
2025-12-25 12:16:45 +0000

    More Avalonia integration work.

---
+++ Source/Runtime/UOEngine.Runtime.AvaloniaUI/AvaloniaControl.cs
@@ -0,0 +1,11 @@
+﻿// Copyright (c) 2025 UOEngine Project, Scotty1234
+// Licensed under the MIT License. See LICENSE file in the project root for details.
+namespace UOEngine.Runtime.AvaloniaUI;
+
+internal class AvaloniaControl
+{
+    public void Initialise()
+    {
+
+    }
+}
