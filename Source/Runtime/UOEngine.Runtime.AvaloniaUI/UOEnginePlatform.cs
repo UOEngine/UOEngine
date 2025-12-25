727a538859beb77e420621d04eb13415c0603a69
Scott Dunkley <scottdunkley@gmail.com>
2025-12-25 03:33:24 +0000

    Added WIP AvaloniaUI initialisation that seems to be required.

---
+++ Source/Runtime/UOEngine.Runtime.AvaloniaUI/UOEnginePlatform.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) 2025 UOEngine Project, Scotty1234
+// Licensed under the MIT License. See LICENSE file in the project root for details.
+using Avalonia.Rendering.Composition;
+using Avalonia.Threading;
+
+namespace UOEngine.Runtime.AvaloniaUI;
+
+internal class UOEnginePlatform
+{
+    public static Compositor Compositor
+        => _compositor ?? throw new InvalidOperationException("Compositor hasn't been initialized");
+
+    private static Compositor? _compositor;
+
+    public static void Initialise()
+    {
+        AvaloniaSynchronizationContext.AutoInstall = false;
+
+        //IPlatformGraphics
+
+        //_compositor = new Compositor(platformGraphics);
+    }
+}
