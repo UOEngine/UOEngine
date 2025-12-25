96b5d4fa48730e7195511fa334bc228fbb8e815d
Scott Dunkley <scottdunkley@gmail.com>
2025-12-25 03:33:24 +0000

    Added WIP AvaloniaUI initialisation that seems to be required.

---
+++ Source/Runtime/UOEngine.Runtime.AvaloniaUI/AvaloniaApp.cs
@@ -0,0 +1,9 @@
+﻿// Copyright (c) 2025 UOEngine Project, Scotty1234
+// Licensed under the MIT License. See LICENSE file in the project root for details.
+using Avalonia;
+
+namespace UOEngine.Runtime.AvaloniaUI;
+
+internal class AvaloniaApp: Application
+{
+}
--- Source/Runtime/UOEngine.Runtime.AvaloniaUI/AvaloniaUIPlugin.cs
+++ Source/Runtime/UOEngine.Runtime.AvaloniaUI/AvaloniaUIPlugin.cs
@@ -1,8 +1,29 @@
 ﻿// Copyright (c) 2025 UOEngine Project, Scotty1234
 // Licensed under the MIT License. See LICENSE file in the project root for details.
+using Avalonia;
+
+using UOEngine.Runtime.Plugin;
+
 namespace UOEngine.Runtime.AvaloniaUI;
 
-public class AvaloniaUIPlugin
+[PluginEntry]
+[PluginLoadingPhase(PluginLoadingPhase.Runtime)]
+public class AvaloniaUIPlugin: IPlugin
 {
+    public void PostStartup()
+    {
+        AppBuilder.Configure<AvaloniaApp>()
+            .UseUOEngine()
+            .SetupWithoutStarting();
+    }
 
+}
+
+public static class AppBuilderExtensions
+{
+    public static AppBuilder UseUOEngine(this AppBuilder builder)
+        => builder
+            .UseStandardRuntimePlatformSubsystem()
+            .UseSkia()
+            .UseWindowingSubsystem(UOEnginePlatform.Initialise);
 }
