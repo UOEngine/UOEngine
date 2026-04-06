1735dfd56ab7bf6ec495b3503a0587636531fc19
Scott Dunkley <scottdunkley@gmail.com>
2026-04-06 22:19:06 +0100

    Basic working landing/home page and Centred placeholder.

--- Source/Editor/UOEngine.Editor.UI/ViewModels/EditorMainViewModel.cs
+++ Source/Editor/UOEngine.Editor.UI/ViewModels/EditorMainViewModel.cs
@@ -1,10 +1,12 @@
 ﻿// Copyright (c) 2026 UOEngine Project, Scotty1234
 // Licensed under the MIT License. See LICENSE file in the project root for details.
 using System.Collections.ObjectModel;
+using System.Windows.Input;
 
 using Avalonia.Controls;
 using Avalonia.Media;
 using Avalonia.Media.Imaging;
+
 using UOEngine.Editor.Abstractions;
 using UOEngine.Runtime.AvaloniaUI;
 
@@ -28,25 +30,31 @@ internal class EditorMainViewModel: UOEngineViewModel
 
         foreach(var tool in editorTools)
         {
-            tools.Add(new ToolViewModel
+            ToolViewModel toolViewModel = null!;
+
+            toolViewModel = new ToolViewModel
             {
                 Name = tool.Name,
                 CanClose = true,
-                Icon = File.Exists(tool.Icon) ? new Bitmap(tool.Icon) : null
+                Icon = File.Exists(tool.Icon) ? new Bitmap(tool.Icon) : null,
+                OpenCommand = new DelegateCommand(() => OpenTool(toolViewModel))
+
                 //Content = tool.CreateContent
-            });
+            };
+
+            tools.Add(toolViewModel);
         }
 
         _editorLandingViewModel = new EditorLandingViewModel(tools); 
 
         Tools[0] = new ToolViewModel
         {
-            Name = "",
+            Name = "Home",
             Content = new EditorLandingView
             {
                 DataContext = _editorLandingViewModel
             },
-            CanClose = false
+            CanClose = false,
         };
 
         SelectedTool = Tools[0];
@@ -60,6 +68,23 @@ internal class EditorMainViewModel: UOEngineViewModel
             i++;
         }
     }
+    private void OpenTool(ToolViewModel tool)
+    {
+        var existing = OpenedTools.FirstOrDefault(x => x == tool || x.Name == tool.Name);
+
+        if (existing is not null)
+        {
+            SelectedTool = existing;
+            OnPropertyChanged(nameof(SelectedTool));
+            return;
+        }
+
+        //tool.Content ??= tool.CreateContent?.Invoke();
+
+        OpenedTools.Add(tool);
+        SelectedTool = tool;
+        OnPropertyChanged(nameof(SelectedTool));
+    }
 }
 
 internal class ToolViewModel
@@ -71,4 +96,5 @@ internal class ToolViewModel
 
     internal bool CanClose { get; set; }
 
+    internal ICommand? OpenCommand { get; set; }
 }
